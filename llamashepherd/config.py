a046333565938e7f9f28ad5dbb105a9062f3fdd8
mikepapadim <mikepapadim@hotmail.com>
2023-12-26 11:12:42 +0200

    add more llama2 implementations

--- llamashepherd/config.py
+++ llamashepherd/config.py
@@ -8,96 +8,100 @@ options = {
         {"name": "llama2.rs", "url": "https://github.com/lintian06/llama2.rs", "author": "@lintian06"},
         {"name": "pecca.rs", "url": "https://github.com/rahoua/pecca-rs", "author": "@rahoua"},
         {"name": "llama2.rs", "url": "https://github.com/flaneur2020/llama2.rs", "author": "@flaneur2020"},
-        # Add other Rust options here
+        # Add other Rust implementations here
     ],
     "Go": [
         {"name": "go-llama2", "url": "https://github.com/tmc/go-llama2", "author": "@tmc"},
         {"name": "llama2.go", "url": "https://github.com/nikolaydubina/llama2.go", "author": "@nikolaydubina"},
         {"name": "llama2.go", "url": "https://github.com/haormj/llama2.go", "author": "@haormj"},
         {"name": "llama2.go", "url": "https://github.com/saracen/llama2.go", "author": "@saracen"},
-        # Add other Go options here
+        # Add other Go implementations here
     ],
     "Android": [
         {"name": "llama2.c-android", "url": "https://github.com/Manuel030/llama2.c-android", "author": "@Manuel030"},
         {"name": "llama2.c-android-wrapper", "url": "https://github.com/celikin/llama2.c-android-wrapper", "author": "@celikin"},
-        # Add other Android options here
+        # Add other Android implementations here
     ],
-    # Add other language categories and options here
+    # Add other language categories and implementations here
     "C++": [
         {"name": "llama2.cpp", "url": "https://github.com/leloykun/llama2.cpp", "author": "@leloykun"},
         {"name": "llama2.cpp", "url": "https://github.com/coldlarry/llama2.cpp", "author": "@coldlarry"},
-        # Add other C++ options here
+        # Add other C++ implementations here
+    ],
+    "CUDA": [
+        {"name": "llama_cu_awq", "url": "https://github.com/ankan-ban/llama_cu_awq", "author": "@ankan-ban"},
+        # Add other CUDA implementations here
     ],
     "JavaScript": [
         {"name": "llama2.js", "url": "https://github.com/epicure/llama2.js", "author": "@epicure"},
         {"name": "llamajs", "url": "https://github.com/agershun/llamajs", "author": "@agershun"},
         {"name": "llama2.ts", "url": "https://github.com/wizzard0/llama2.ts", "author": "@oleksandr_now"},
         {"name": "llama2.c-emscripten", "url": "https://github.com/gohai/llama2.c-emscripten", "author": "@gohai"},
-        # Add other JavaScript options here
+        # Add other JavaScript implementations here
     ],
     "Zig": [
         {"name": "llama2.zig", "url": "https://github.com/cgbur/llama2.zig", "author": "@cgbur"},
         {"name": "llama2.zig", "url": "https://github.com/vodkaslime/llama2.zig", "author": "@vodkaslime"},
         {"name": "llama2.zig", "url": "https://github.com/clebert/llama2.zig", "author": "@clebert"},
-        # Add other Zig options here
+        # Add other Zig implementations here
     ],
     "Julia": [
         {"name": "llama2.jl", "url": "https://github.com/juvi21/llama2.jl", "author": "@juvi21"},
-        # Add other Julia options here
+        # Add other Julia implementations here
     ],
     "Scala": [
         {"name": "llama2.scala", "url": "https://github.com/jrudolph/llama2.scala", "author": "@jrudolph"},
-        # Add other Scala options here
+        # Add other Scala implementations here
     ],
     "Java": [
         {"name": "llama2.java", "url": "https://github.com/mukel/llama2.java", "author": "@mukel"},
         {"name": "llama2.tornadovm.java", "url": "https://github.com/mikepapadim/llama2.tornadovm.java", "author": "@mikepapadim"},
         {"name": "Jlama", "url": "https://github.com/tjake/Jlama", "author": "@tjake"},
         {"name": "llama2j", "url": "https://github.com/LastBotInc/llama2j", "author": "@lasttero"},
-        # Add other Java options here
+        # Add other Java implementations here
     ],
     "Kotlin": [
         {"name": "llama2.kt", "url": "https://github.com/madroidmaq/llama2.kt", "author": "@madroidmaq"},
-        # Add other Kotlin options here
+        # Add other Kotlin implementations here
     ],
     "Python": [
         {"name": "llama2.py", "url": "https://github.com/tairov/llama2.py", "author": "@tairov"},
-        # Add other Python options here
+        # Add other Python implementations here
     ],
     "C#": [
         {"name": "llama2.cs", "url": "https://github.com/trrahul/llama2.cs", "author": "@trrahul"},
-        # Add other C# options here
+        # Add other C# implementations here
     ],
     "Dart": [
         {"name": "llama2.dart", "url": "https://github.com/yiminghan/llama2.dart", "author": "@yiminghan"},
-        # Add other Dart options here
+        # Add other Dart implementations here
     ],
     "Web": [
         {"name": "llama2c-web", "url": "https://github.com/dmarcos/llama2.c-web", "author": "@dmarcos"},
-        # Add other Web options here
+        # Add other Web implementations here
     ],
     "WebAssembly": [
         {"name": "icpp-llm", "url": "https://github.com/icppWorld/icpp-llm"},
-        # Add other WebAssembly options here
+        # Add other WebAssembly implementations here
     ],
     "Fortran": [
         {"name": "llama2.f90", "url": "https://github.com/rbitr/llama2.f90"},
-        # Add other Fortran options here
+        # Add other Fortran implementations here
     ],
     "Mojo": [
         {"name": "llama2.🔥", "url": "https://github.com/tairov/llama2.mojo", "author": "@tairov"},
-        # Add other Mojo options here
+        # Add other Mojo implementations here
     ],
     "OCaml": [
         {"name": "llama2.ml", "url": "https://github.com/jackpeck/llama2.ml", "author": "@jackpeck"},
-        # Add other OCaml options here
+        # Add other OCaml implementations here
     ],
     "Everywhere": [
         {"name": "llama2.c", "url": "https://github.com/trholding/llama2.c", "author": "@trholding"},
-        # Add other "Everywhere" options here
+        # Add other "Everywhere" implementations here
     ],
     "Bilingual": [
         {"name": "llama2.c-zh", "url": "https://github.com/chenyangMl/llama2.c-zh", "author": "@chenyangMl"},
-        # Add other "Bilingual" options here
+        # Add other "Bilingual" implementations here
     ],
 }
